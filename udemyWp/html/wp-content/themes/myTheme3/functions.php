042c3e00c24f11229d3df6873ce3f1ee5e048005
ducksfrogs <ma_yamaki@yahoo.com>
2024-08-25 16:35:17 +0900

    lazy

--- udemyWp/html/wp-content/themes/myTheme3/functions.php
+++ udemyWp/html/wp-content/themes/myTheme3/functions.php
@@ -1,6 +1,11 @@
 <?php 
 
 function univ_files() {
-    wp_enqueue_style( 'univ_main_style', get_stylesheet_uri(  ) );
+
+    wp_enqueue_script( 'main-js', get_theme_file_uri( '/assets/scripts' ), array('jquery'), 1.0);
+    wp_enqueue_style( 'font-awesome',  '//font-awesome.com/ffff' );
+    wp_enqueue_style( 'univ_main_style', get_theme_file_uri( '/assets/styles/style.css' ));
+    wp_enqueue_style( 'univ_main_style', get_theme_file_uri( '/assets/styles/style.css' ));
+    wp_enqueue_style( 'univ_extra_style', get_theme_file_uri( '/assets/styles/style_ectra.css' ));
 }
 add_action( 'wp_enqueue_scripts', 'univ_files' );
