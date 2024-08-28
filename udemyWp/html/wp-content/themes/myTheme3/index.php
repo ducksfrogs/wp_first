af0fa8d4b21acd08f17d6e7b139131f71cc175a4
ducksfrogs <ma_yamaki@yahoo.com>
2024-08-29 05:57:38 +0900

    about us

--- udemyWp/html/wp-content/themes/myTheme3/index.php
+++ udemyWp/html/wp-content/themes/myTheme3/index.php
@@ -19,7 +19,11 @@ get_header(  );
           <div class="post-item">
             <h2><a href="<?php the_permalink(  ); ?>"><?php the_title(  ) ?></a></h2>
             <div class="meta-box">
-              <p>Posted by </p>
+              <p>Posted by <?php the_author_posts_link(  ); ?> on <?php the_time('Y'); ?> in <?php echo get_the_category_list( ', ' ); ?></p>
+            </div>
+            <div class="generic-content">
+              <?php the_excerpt(  ); ?>
+              <p><a class = 'btn btn--blue" href="<?php the_parmalink(); ?>">Contine readeng</a></p>
             </div>
           </div>
 
