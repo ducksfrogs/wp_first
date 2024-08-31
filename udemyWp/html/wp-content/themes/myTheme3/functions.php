92604149085da320b65f3df9cd5987070cff777b
ducksfrogs <ma_yamaki@yahoo.com>
2024-08-31 11:30:42 +0900

    query selector

--- udemyWp/html/wp-content/themes/myTheme3/functions.php
+++ udemyWp/html/wp-content/themes/myTheme3/functions.php
@@ -18,3 +18,16 @@ function univ_features() {
     add_theme_support( 'title-tag' );
 }
 add_action( 'after_setup_theme', 'univ_features' );
+
+
+function univ_post_types () {
+    register_post_type('event', array(
+        'public' => true,
+        'labels' => array(
+            'name' => 'Events'
+        ),
+        'menu_icon' => 'dashicons-calendar'
+    ));
+
+}
+add_action('init', 'univ_post_types');
