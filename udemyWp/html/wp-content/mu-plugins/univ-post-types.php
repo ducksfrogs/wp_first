c1f48ae8a44da3520d9c56e6f6052bf340f52c57
ducksfrogs <ma_yamaki@yahoo.com>
2024-08-31 18:14:01 +0900

    query selector

--- udemyWp/html/wp-content/mu-plugins/univ-post-types.php
+++ udemyWp/html/wp-content/mu-plugins/univ-post-types.php
@@ -1,17 +1,36 @@
-<?php
-function univ_post_types () {
-    register_post_type('event', array(
+<?php 
+// function univ_post_types () {
+//     register_post_type('event', array(
+//         'public' => true,
+//         'labels' => array(
+//             'name' => 'Events',
+//             'add_new_item' => 'Add NEw Event',
+//             'edit_item' => 'edit event',
+//             'all_items' => 'All Events',
+//             'sigular_name' => 'Event'
+//         ),
+//         'menu_icon' => 'dashicons-calendar'
+//     ));
+
+// }
+// add_action('init', 'univ_post_types');
+
+     
+    function university_post_types() {
+      register_post_type('event', array(
+        'rewrite' => array('slug' => 'events'),
+        'has_archive' => true,
         'public' => true,
+        // 'show_in_rest' => true,
         'labels' => array(
-            'name' => 'Events',
-            'add_new_item' => 'Add NEw Event',
-            'edit_item' => 'edit event',
-            'all_items' => 'All Events',
-            'sigular_name' => 'Event'
+          'name' => 'Events',
+          'add_new_item' => 'Add New Event',
+          'edit_item' => 'Edit Event',
+          'all_items' => 'All Events',
+          'singular_name' => 'Event'
         ),
         'menu_icon' => 'dashicons-calendar'
-    ));
-
-}
-add_action('init', 'univ_post_types');
-
+      ));
+    }
+     
+    add_action('init', 'university_post_types');
