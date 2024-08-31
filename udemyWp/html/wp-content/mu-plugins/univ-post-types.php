7cf96ab0b8e763448960f2432f66550883f70dac
ducksfrogs <ma_yamaki@yahoo.com>
2024-08-31 16:17:23 +0900

    query selector

--- udemyWp/html/wp-content/mu-plugins/univ-post-types.php
+++ udemyWp/html/wp-content/mu-plugins/univ-post-types.php
@@ -3,7 +3,11 @@ function univ_post_types () {
     register_post_type('event', array(
         'public' => true,
         'labels' => array(
-            'name' => 'Events'
+            'name' => 'Events',
+            'add_new_item' => 'Add NEw Event',
+            'edit_item' => 'edit event',
+            'all_items' => 'All Events',
+            'sigular_name' => 'Event'
         ),
         'menu_icon' => 'dashicons-calendar'
     ));
