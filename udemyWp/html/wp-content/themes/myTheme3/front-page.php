bc0552c548210c54debe550b5b8cef5972ed5ccf
ducksfrogs <ma_yamaki@yahoo.com>
2024-09-01 10:02:34 +0900

    query selector

--- udemyWp/html/wp-content/themes/myTheme3/front-page.php
+++ udemyWp/html/wp-content/themes/myTheme3/front-page.php
@@ -15,9 +15,21 @@
           <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
           <?php 
+            $today = date('Ymd');
             $homepageEvents = new WP_Query(array(
-              'posts_per_page' => 2,
+              'posts_per_page' => -1,
               'post_type' => 'event',
+              'meta_key' => 'event_date',
+              'orderby' => 'meta_value_num',
+              'order' => 'ASC',
+              'meta_query' => array(
+                array( 
+                  'key' => 'event_date',
+                  'compare' => '>=',
+                  'value' => $today,
+                  'type' => 'numeric' 
+                )
+              )
             )
             );
 
@@ -30,7 +42,10 @@
                 $eventDate = new DateTime(get_field('event_date'));
                 echo $eventDate -> format('M')
                ?> </span>
-              <span class="event-summary__day">25</span>
+              <span class="event-summary__day"><?php
+                $eventDate = new DateTime(get_field('event_date'));
+                echo $eventDate -> format('d')
+                ?></span>
             </a>
             <div class="event-summary__content">
               <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>
@@ -41,7 +56,6 @@
             }
 
           ?>
-          uP coming events.
 
           <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
         </div>
@@ -51,7 +65,7 @@
           <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
           <?php 
             $homepagePosts = new WP_Query(array(
-              'posts_per_page' => 1,
+              'posts_per_page' => 2,
               // 'category_name' => 'awards'
               ));
             
